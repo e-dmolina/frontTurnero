1abac9a84af9c1bf0002d5d3a19e64002ad87911
e-dmolina <diegomolina1983@gmail.com>
2020-05-22 17:30:41 -0300

    compilacio con nuevo ip

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4713680c35a84c40647454015cfa5dc9.js"
+  "/precache-manifest.7a0005443564dc4c6545e86c87ec2e69.js"
 );
 
 self.addEventListener('message', (event) => {
