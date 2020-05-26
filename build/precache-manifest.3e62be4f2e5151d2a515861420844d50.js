e089d21e1e3d54678577110167903e289284b0f9
Diego Molina <diegomolina1983@gmail.com>
2020-05-26 16:17:07 -0300

    genera nueva compilacion

---
+++ build/precache-manifest.3e62be4f2e5151d2a515861420844d50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f048f5dcd7b1519e11ede35d18a0be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e558a72e93de536801",
+    "url": "/static/js/2.cbd4b511.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.cbd4b511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9896433ce0e2a0ec58",
+    "url": "/static/js/main.556beb4a.chunk.js"
+  },
+  {
+    "revision": "babe4e912d3f977fe5ed",
+    "url": "/static/js/runtime-main.a227e973.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a0005443564dc4c6545e86c87ec2e69.js"
+  "/precache-manifest.3e62be4f2e5151d2a515861420844d50.js"
 );
 
 self.addEventListener('message', (event) => {
